69dc28b9c437b51b7187902376c45b40f81a516e
Philipp Falke <philipp.falke@meteoswiss.ch>
2020-04-01 10:29:05 +0200

    fix: Migrated repo helper files to comply with new MeteoSwiss standards

---
+++ AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Philipp Falke <philipp.falke@meteoswiss.ch>
+
+Contributors
+------------
+
+None yet. Why not be the first?
--- setup.py
+++ setup.py
@@ -6,19 +6,61 @@ from setuptools import setup, find_packages
 
 import versioneer
 
+requirements = [
+    'numpy',
+]
+setup_requirements = [
+    'setuptools_scm',
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+extras = {
+    'test': test_requirements,
+}
+
+packages = find_packages(include=['opendap_protocol'])
+
+package_dir = {}
+
+package_data = {}
+
+
 setup(
     name='opendap-protocol',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author='Philipp Meier',
-    author_email='philipp.meier@meteoswiss.ch',
+    author="Philipp Falke",
+    author_email='philipp.falke@meteoswiss.ch',
     description='A pure Python implementation of the OPeNDAP server protocol.',
-    packages=find_packages(),
+    url='https://github.com/MeteoSwiss/opendap-protocol',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD-3-Clause License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='opendap_protocol',
     entry_points={},
-    py_modules=['opendap_protocol'],
+    scripts=[],
+    license="BSD-3-Clause license",
+    long_description=open('README.md').read() + '\n\n' +
+                     open('HISTORY.rst').read(),
     include_package_data=True,
-    license='MIT License',
-    long_description=open('README.md').read(),
-    install_requires=[
-        'numpy',
-    ])
+    zip_safe=False,
+    test_suite='test',
+    py_modules=['opendap-protocol'],
+    packages=packages,
+    install_requires=requirements,
+    package_dir=package_dir,
+    package_data=package_data,
+    setup_requires=setup_requirements,
+    tests_require=test_requirements,
+    extras_require=extras,
+    )
